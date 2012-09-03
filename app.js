e6c09c685fadb442d29751a2a63a2392596eb28b
Michael Laccetti <michael@laccetti.com>
2012-09-03 13:42:00 -0400

    Error handling
    * jacked in some handy dandy error handling (okay, it's actually brutal
    and ugly as sin, but hey, it's not like I *know* how to write JS code).

--- app.js
+++ app.js
@@ -4,90 +4,104 @@ var pg = require('pg');
 
 // config
 app.configure(function () {
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.compiler({ src:__dirname + '/public', enable:['less']}));
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.compiler({ src:__dirname + '/public', enable:['less']}));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
 // disable logging
 io.set('log level', 1);
 
 app.configure('development', function () {
-    app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
+  app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
 });
 
 app.configure('production', function () {
-    app.use(express.errorHandler());
+  app.use(express.errorHandler());
 });
 
 // Connect to the DB
-var conString = "tcp://powerdns:powerdns2k10^^@plop/powerdns";
-var client = new pg.Client(conString);
-client.connect();
+var connString = "tcp://powerdns:powerdns2k10^^@plop/powerdns";
+
+var client = null, connected = false, pg_err;
+pg.connect(connString, function (err, pg_client) {
+  if (err == null) {
+    console.log("Connected to PG.");
+    connected = true;
+    client = pg_client;
+  } else {
+    pg_err = err;
+    console.log("Could not connect to PG. " + err);
+  }
+});
 
 // Routes
 app.get('/', routes.index);
 
 app.listen(9090, function () {
-    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
 io.sockets.on('connection', function (socket) {
-    socket.on('sql', function (data) {
-        console.log('Executing SQL statement: ' + data);
+  socket.on('sql', function (data) {
+    console.log('Executing SQL statement: ' + data);
+    query_pg(socket, data);
+  });
 
-        var query = client.query(data);
-        var rows = [];
+  socket.on('list', function () {
+    console.log("Listing available tables.");
+    query_pg(socket, "select table_catalog, table_schema, table_name from information_schema.tables where table_type='BASE TABLE' and is_insertable_into='YES' and table_schema not in ('pg_catalog', 'information_schema') order by table_name;");
+  });
 
-        query.on('row', function(row) {
-            rows.push(row);
-        });
-
-        query.on('end', function(result) {
-            //fired once and only once, after the last row has been returned and after all 'row' events are emitted
-            //in this example, the 'rows' array now contains an ordered set of all the rows which we received from postgres
-            console.log(result.rowCount + ' rows were received');
-            socket.emit('sql-output', rows);
-        })
-    });
-
-    socket.on('list', function() {
-        console.log("Listing available tables.");
-        var query = client.query("select table_catalog, table_schema, table_name from information_schema.tables where table_type='BASE TABLE' and is_insertable_into='YES' and table_schema not in ('pg_catalog', 'information_schema') order by table_name;");
-        var rows = [];
-
-        query.on('row', function(row) {
-            rows.push(row);
-        });
-
-        query.on('end', function(result) {
-            //fired once and only once, after the last row has been returned and after all 'row' events are emitted
-            //in this example, the 'rows' array now contains an ordered set of all the rows which we received from postgres
-            console.log(result.rowCount + ' rows were received');
-            socket.emit('sql-output', rows);
-        })
-    });
-
-    socket.on('describe', function(data) {
-        var table = data.split(" ")[1];
-        console.log("Describing the table structure of: " + table);
-
-        var query = client.query("select table_catalog, table_schema, table_name, column_name, ordinal_position, column_default, is_nullable, data_type from INFORMATION_SCHEMA.COLUMNS where table_name = '" + table + "';")
-        var rows = [];
-
-        query.on('row', function(row) {
-            rows.push(row);
-        });
-
-        query.on('end', function(result) {
-            //fired once and only once, after the last row has been returned and after all 'row' events are emitted
-            //in this example, the 'rows' array now contains an ordered set of all the rows which we received from postgres
-            console.log(result.rowCount + ' rows were received');
-            socket.emit('sql-output', rows);
-        })
-    });
+  socket.on('describe', function (data) {
+    var table = data.split(" ")[1];
+    console.log("Describing the table structure of: " + table);
+    query_pg(socket, "select table_catalog, table_schema, table_name, column_name, ordinal_position, column_default, is_nullable, data_type from INFORMATION_SCHEMA.COLUMNS where table_name = '" + table + "';");
+  });
 });
+
+function query_pg(socket, sql_query) {
+  var hadError = false, error;
+
+  if (connected) {
+    try {
+
+      var query = client.query(sql_query);
+      var rows = [];
+
+      query.on('error', function (pg_error) {
+        console.log("PG returned an error: " + pg_error);
+        hadError = true;
+        error = "<p>There was an error issuing the query: " + pg_error + "</p>";
+      });
+
+      if (!hadError) {
+        query.on('row', function (row) {
+          rows.push(row);
+        });
+
+        query.on('end', function (result) {
+          if (result != null) {
+            console.log(result.rowCount + ' rows were received');
+            socket.emit("sql-output", rows, false);
+          }
+
+          if (hadError) {
+            socket.emit("sql-output", error, true)
+          }
+        })
+      }
+    } catch (exception) {
+      console.log("PG threw an exception: " + exception);
+      hadError = true;
+      error = "<p>There was a problem with issuing the query: " + exception.message + "</p>";
+    }
+  } else {
+    console.log("Not connect to PG, cannot execute query.");
+    error = "<p>There was a problem connecting to the PG database: " + pg_err + "</p>";
+  }
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,59 +1,62 @@
 // Hack some stuff into the String object
 if (typeof String.prototype.startsWith != 'function') {
-    String.prototype.startsWith = function (str){
-        return this.lastIndexOf(str, 0) === 0;
-    }
+  String.prototype.startsWith = function (str) {
+    return this.lastIndexOf(str, 0) === 0;
+  }
 }
 
 var socket = io.connect('http://localhost:9090');
 
-$(document).ready(function()
-{
+$(document).ready(function () {
   $('#terminal-entry').show();
   $('#entry').focus();
 });
 
-$('#entry').on('keypress', function(event) {
-    if(event.which == 13) {
-        event.preventDefault();
-        var message = new String($('#entry').val());
+$('#entry').on('keypress', function (event) {
+  if (event.which == 13) {
+    event.preventDefault();
+    var message = new String($('#entry').val());
 
-        if (message.startsWith("help")) {
-            $('#sql-data').html("<h1>help</h1><p>Currently available functionality: </p><p><blockquote>* any SQL query you can think of<br/>*\\l - list tables<br/>*\\d &lt;table name&gt; - describe a table</blockquote></p>");
-        } else if (message.startsWith("\\d")) {
-            socket.emit('describe', message);
-        } else if (message.startsWith("\\l")) {
-            socket.emit('list');
-        } else if (message.toLowerCase() === 'clear') {
-          $('#sql-data').html('');
-        } else {
-            socket.emit('sql', message);
-            return false;
-        }
+    if (message.startsWith("help")) {
+      $('#sql-data').html("<h1>help</h1><p>Currently available functionality: </p><p><blockquote>* any SQL query you can think of<br/>*\\l - list tables<br/>*\\d &lt;table name&gt; - describe a table</blockquote></p>");
+    } else if (message.startsWith("\\d")) {
+      socket.emit('describe', message);
+    } else if (message.startsWith("\\l")) {
+      socket.emit('list');
+    } else if (message.toLowerCase() === 'clear') {
+      $('#sql-data').html('');
+    } else {
+      socket.emit('sql', message);
+      return false;
     }
+  }
 });
 
-socket.on('sql-output', function(data) {
+socket.on('sql-output', function (data, is_err) {
+  if (!is_err) {
     var headers = "<tr>";
     var rows = "";
 
     if (data.length === 0) {
-        $("#sql-data").html("<h1>results</h1><div>No results for the provided query.</div>");
-        return;
+      $("#sql-data").html("<h1>results</h1><div>No results for the provided query.</div>");
+      return;
     }
 
-    $.each(data, function(idx, row) {
-        rows += "<tr>";
-        $.each(row, function (cellName, cellValue) {
-           if (idx == 0) {
-               headers += "<th>" + cellName + "</th>";
-           }
-           rows += "<td>" + cellValue + "</td>";
-        });
-        rows += "</tr>";
+    $.each(data, function (idx, row) {
+      rows += "<tr>";
+      $.each(row, function (cellName, cellValue) {
+        if (idx == 0) {
+          headers += "<th>" + cellName + "</th>";
+        }
+        rows += "<td>" + cellValue + "</td>";
+      });
+      rows += "</tr>";
     });
 
     headers += "</tr>";
 
     $("#sql-data").html("<h1>results</h1><table>" + headers + rows + "</table>");
+  } else {
+    $("#sql-data").html("<h1>error</h1>" + data);
+  }
 });
